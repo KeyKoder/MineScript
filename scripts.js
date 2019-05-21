453f99487cef9214b9844da33c9ab55f4ed2c7d7
KeyKoder <victor.fernandez.montano@gmail.com>
2019-05-21 22:46:23 +0200

    Change highlighting (Fix #1)
    Change minimal aspects of the syntax highlighting.

--- scripts.js
+++ scripts.js
@@ -1,20 +1,17 @@
 $(document).ready(function(){
     setInterval(highlight,1);
 
-    $("#highlight").click(function(){
-        $("#code").focus();
-    })
-
     $("#code").keydown(function(){
         $("#highlight").text($(this).val());
     })
     $("#code").keyup(function(){
         $("#highlight").text($(this).val());
     })
+
 })
 
 function highlight(){
-    let codeElements = $("code");
+    let codeElements = $(".code");
     let primaryKeyReg=/\b(let|new)(?=[^\w])/g;
     let functionReg=/\b(FloorCrafting|as|at)(?=[^\w])/g;
     let classesReg=/\b(Item|Selector)(?=[^\w])/g;
@@ -24,17 +21,20 @@ function highlight(){
     let stringReg=/((?<![\\])['"])((?:.(?!(?<![\\])\1))*.?)\1/g; //From: https://www.metaltoad.com/blog/regex-quoted-string-escapable-quotes
     // let stringRegSingle=/'(.*?)'/g;
     let commentReg=/(\/\/.*)/g
+    let blockCommentReg=/(\/\*(.|\n)+\*\/)/g
     codeElements.each(function(){
         var string = $(this).text();
         var parsed = string.replace(ltReg,'&lt;');
         parsed = parsed.replace(gtReg,'&gt;');
         parsed = parsed.replace(htmlReg,'<pre>$1</pre>');
         parsed = parsed.replace(stringReg,'<span class="string">$1$2$1</span>');
-        // parsed = parsed.replace(stringRegSingle,'<span class="string">\'$1\'</span>');
+        parsed = parsed.replace(commentReg,'<span class="comment">$1</span>');
+        parsed = parsed.replace(blockCommentReg,'<span class="comment">$1</span>');
         parsed = parsed.replace(primaryKeyReg,'<span class="primary">$1</span>');
         parsed = parsed.replace(functionReg,'<span class="function">$1</span>');
         parsed = parsed.replace(classesReg,'<span class="class">$1</span>');
-        parsed = parsed.replace(commentReg,'<span class="comment">$1</span>');
         $(this).html(parsed)
+        $(this).find(".string").find("span").attr("class","");
+        $(this).find(".comment").find("span").attr("class","");
     })
 }
